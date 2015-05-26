07091f5ba295ae02b46a5e6ca27134def345f505
txx3258 <soft_tangxiaoxian@163.com>
2015-05-26 08:29:15 +0800

    admin ejs for error

--- routes/developer.js
+++ routes/developer.js
@@ -12,7 +12,8 @@ router.get('/login', function(req, res, next) {
 
     if (userName=="admin@163.com"&&userPwd=="admin"){
         res.render('developer/manage', { title: '玄魂的测试代码',
-            staticResourceUrl: constants.staticResourceHost
+            staticResourceUrl: constants.staticResourceHost,
+            mClass:"manage"
         }) ;
     }else{
         res.render('admin/login', { title: '玄魂的测试代码',
