bbbe2bd4c76cf059132be96eb67363072515e17b
txx3258 <soft_tangxiaoxian@163.com>
2015-05-27 08:42:47 +0800

    admin:list

--- public/js/admin.js
+++ public/js/admin.js
@@ -15,11 +15,40 @@
         }else{
             clearInterval(mgsBoxInterval)
         }
-        console.log(msgBoxHt)
+        //console.log(msgBoxHt)
     },200);
 
     $('#msgBoxClose').click(function(){
         $('#msgBox').hide();
         $('#msgBoxBody').remove();
-    })
+    });
+
+    jQuery.fn.center = function () {
+            this.css('position','absolute');
+            this.css('top', ( $(window).height() - this.height() ) /2 +$(window).scrollTop() + 'px');
+            this.css('left', ( $(window).width() - this.width() ) / 2+$(window).scrollLeft() + 'px');
+            return this;
+        }
+
+    $("#bannerBar").center();
+
+
+    $('#banner').mousedown( function (event) {
+
+        var isMove = true;
+        var abs_x = event.pageX - $('#bannerBar').offset().left;
+        var abs_y = event.pageY - $('#bannerBar').offset().top;
+
+        $(document).mousemove(function (event) {
+            if (isMove) {
+                var obj = $('#bannerBar');
+                obj.css({'left':event.pageX - abs_x, 'top':event.pageY - abs_y});
+            }
+        }).mouseup(function () {
+            isMove = false;
+        });
+
+    });
+
 })($,window);
+
